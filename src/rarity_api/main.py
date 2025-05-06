e7db2b26fc413685ac71a1feb7c8eac711e6bee3
artem <artem@deffun.io>
2025-05-06 11:09:36 +0700

    fix certs renewal

--- src/rarity_api/main.py
+++ src/rarity_api/main.py
@@ -4,6 +4,7 @@ from alembic import command
 from alembic.config import Config
 from fastapi import FastAPI
 
+from rarity_api.common.http_client import HttpClient
 from rarity_api.endpoints.city_router import router as city_router
 from rarity_api.endpoints.country_router import router as country_router
 from rarity_api.endpoints.item_router import router as item_router
@@ -11,6 +12,7 @@ from rarity_api.endpoints.manufacturer_router import router as manufacturer_rout
 from rarity_api.endpoints.region_router import router as region_router
 from rarity_api.endpoints.search_history_router import router as search_history_router
 from rarity_api.google_auth.router import router as google_auth_router
+from rarity_api.google_auth.utils.id_provider_certs import IdentityProviderCerts
 from rarity_api.native_auth.router import router as plain_auth_router
 
 app = FastAPI(
@@ -32,8 +34,12 @@ app.include_router(search_history_router)
 
 @asynccontextmanager
 async def lifespan():
+    http_client = HttpClient()
+    await http_client.init_session()
+    await IdentityProviderCerts().renew_certs()
     run_migrations()
     yield
+    await http_client.close_session()
 
 
 def run_migrations():
