c0f6438efdc670c9458b37c161133c19ad3b1abc
w1tnessbtwwwwww <work.w1tnessbtw@mail.ru>
2025-05-09 20:20:40 +0700

    hotfix base import

--- alembic/env.py
+++ alembic/env.py
@@ -19,7 +19,7 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from src.rarity_api.core.models.models import Base
+from src.rarity_api.core.database.models.models import Base
 target_metadata = Base.metadata
 # target_metadata = None
 
