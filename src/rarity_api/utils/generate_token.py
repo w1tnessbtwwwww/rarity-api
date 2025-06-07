475200452dea3e53228ff59c02bd6794b26e730d
w1tnessbtwwwwww <work.w1tnessbtw@mail.ru>
2025-06-07 23:26:02 +0700

    hotfix datetime

--- src/rarity_api/utils/generate_token.py
+++ src/rarity_api/utils/generate_token.py
@@ -1,8 +1,8 @@
 import secrets
-from datetime import datetime, timedelta
-
+from datetime import timedelta
+import datetime
 def generate_confirmation_token():
     return secrets.token_urlsafe(32)
 
 def set_token_expiry():
-    return datetime.now(tz=datetime.timezone.utc) + timedelta(hours=24)  # Токен истекает через 24 часа
+    return datetime.datetime.now(tz=datetime.timezone.utc) + timedelta(hours=24)  # Токен истекает через 24 часа
